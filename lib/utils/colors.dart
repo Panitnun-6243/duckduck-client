cccd3fb27ea176489d4e916b7b329c8e0906fc63
Panitnun-6243 <panitnun.6243@gmail.com>
2023-09-30 23:57:00 +0700

    feat: add custom color

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,3 +1,21 @@
-import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
 
-class DuckDuckColors {}
+class DuckDuckColors {
+  static HexColor duckyYellow = HexColor('#F7DE64');
+  static HexColor caramelCheese = HexColor('#FFF3B7');
+  static HexColor whippedCream = HexColor('#FFFCF0');
+  static HexColor frostWhite = HexColor('#FDFEFD');
+  static HexColor skyBlue = HexColor('#3EB7F4');
+  static HexColor cocoa = HexColor('#6D4A09');
+  static HexColor steelBlack = HexColor('#2A2A2A');
+  static HexColor metalBlue = HexColor('#075881');
+  static HexColor mandarinOrange = HexColor('#F7B364');
+}
+
+class DuckDuckStatus {
+  static HexColor success = HexColor('#83F53D');
+  static HexColor warning = HexColor('#F79964');
+  static HexColor error = HexColor('#F85454');
+  static HexColor disabled = HexColor('#E5E6E5');
+  static HexColor disabledForeground = HexColor('#CBCDCB');
+}
