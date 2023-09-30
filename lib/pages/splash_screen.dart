526a5d928ccc126ac1dbcbfb9ce932625e23e4b2
Panitnun-6243 <panitnun.6243@gmail.com>
2023-10-01 00:35:00 +0700

    feat: create splash screen

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -1,10 +1,34 @@
+import 'package:google_fonts/google_fonts.dart';
+import 'package:duckduck/utils/colors.dart';
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: DuckDuckColors.caramelCheese,
+      body: Center(
+        child: Text(
+          'DuckDuck',
+          style: GoogleFonts.rubik(
+            fontSize: 36,
+            fontWeight: FontWeight.bold,
+            color: DuckDuckColors.cocoa,
+          ),
+        ),
+      ),
+    );
   }
 }
