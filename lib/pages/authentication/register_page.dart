0b65048b7dd854e740056791375f8dcf11b7289a
chutirat <chutirat.earth12@gmai.com>
2023-10-09 21:26:37 +0700

    feat: add register textform

--- lib/pages/authentication/register_page.dart
+++ lib/pages/authentication/register_page.dart
@@ -1,6 +1,10 @@
+import 'package:duckduck/widgets/login/custom_textform.dart';
 import 'package:flutter/material.dart';
+import 'package:duckduck/utils/colors.dart';
+import 'package:duckduck/widgets/authen_button.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-import '../../widgets/login/custom_textform.dart';
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
@@ -10,8 +14,37 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _deviceCodeController = TextEditingController();
+  final FocusNode _emailNode = FocusNode();
+  final FocusNode _passwordNode = FocusNode();
+  final FocusNode _nameNode = FocusNode();
+  final FocusNode _deviceCodeNode = FocusNode();
+  bool isSubmitted = false;
+
+  Future<void> handleLogin() async {
+    setState(() => isSubmitted = true);
+    FocusScope.of(context).unfocus();
+    Navigator.pushNamed(context, '/login');
+  }
+
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    _nameController.dispose();
+    _deviceCodeController.dispose();
+    _emailNode.dispose();
+    _passwordNode.dispose();
+    _nameNode.dispose();
+    _deviceCodeNode.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    double topPadding = MediaQuery.of(context).size.height * 0.16;
+    double bottomPadding = MediaQuery.of(context).size.height * 0.04;
     return Container(
         decoration: const BoxDecoration(
           image: DecorationImage(
@@ -20,29 +53,79 @@ class _RegisterPageState extends State<RegisterPage> {
         ),
         child: Scaffold(
           backgroundColor: Colors.transparent,
-          body: SingleChildScrollView(
-            child: Column(
-              children: [
-                // CustomTextForm(
-                //     controller: _emailController,
-                //     focusNode: _emailNode,
-                //     hintText: 'Email',
-                //     textInputAction: TextInputAction.next,
-                //     onSubmitted: (_) =>
-                //         FocusScope.of(context).requestFocus(_passwordNode)),
-                // const SizedBox(
-                //   height: 15,
-                // ),
-                // CustomTextForm(
-                //   controller: _passwordController,
-                //   focusNode: _passwordNode,
-                //   hintText: 'Password',
-                //   icon: Icons.lock_rounded,
-                //   obscureText: true,
-                // ),
-              ],
+          body: Padding(
+           padding: EdgeInsets.only(
+            top: topPadding, left: 36, right: 36, bottom: bottomPadding),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                
+                SizedBox(
+                  width: double.infinity,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      CustomTextForm(controller: _emailController,
+                      focusNode: _emailNode,
+                      hintText: 'Email',
+                      textInputAction: TextInputAction.next,
+                      onSubmitted: (_) => FocusScope.of(context).requestFocus(_passwordNode)),
+                      const SizedBox(
+                        height: 15 ,
+                      ),
+
+                      CustomTextForm(controller: _passwordController,
+                      focusNode: _passwordNode,
+                      hintText:  'Password',
+                      icon: Icons.lock_rounded,
+                      obscureText: true,
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      Align(
+                        alignment: Alignment.centerRight,
+                        
+                      ),
+
+                      CustomTextForm(controller: _nameController,
+                      focusNode: _nameNode,
+                      hintText: 'Name',
+                      icon: Icons.account_circle_rounded,
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      Align(
+                        alignment: Alignment.centerRight,
+                      ),
+
+                      CustomTextForm(controller: _deviceCodeController,
+                      focusNode: _deviceCodeNode,
+                      hintText: 'Device Node',
+                      icon: Icons.qr_code,
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      Align(
+                        alignment: Alignment.centerRight,
+                      ),
+                      
+                    ],
+                  ),
+                )
+                ],
+
+               
+              ),
             ),
           ),
-        ));
+        );
   }
 }
