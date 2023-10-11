10ddd25c8cd561c1015a4a43d594cd8f9f114d26
Panitnun-6243 <panitnun.6243@gmail.com>
2023-10-11 18:49:21 +0700

    feat: draft home page

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:duckduck/utils/colors.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,9 +11,26 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Homepage'),
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              flex: 1,
+              child: Container(
+                  width: double.infinity,
+                  alignment: Alignment.center,
+                  color: DuckDuckColors.cocoa,
+                  child: const Text('Profile caed')),
+            ),
+            Expanded(
+                flex: 4,
+                child: Container(
+                    width: double.infinity,
+                    color: DuckDuckColors.caramelCheese,
+                    child: const Text('Duck float on water'))),
+          ],
+        ),
       ),
     );
   }
