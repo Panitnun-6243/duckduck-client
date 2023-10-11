14254597a705ca602031c42f632d87de1ec77208
Panitnun-6243 <panitnun.6243@gmail.com>
2023-10-11 18:10:53 +0700

    feat: change hexcolor to material color

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,21 +1,21 @@
-import 'package:hexcolor/hexcolor.dart';
+import 'package:flutter/material.dart';
 
 class DuckDuckColors {
-  static HexColor duckyYellow = HexColor('#F7DE64');
-  static HexColor caramelCheese = HexColor('#FFF3B7');
-  static HexColor whippedCream = HexColor('#FFFCF0');
-  static HexColor frostWhite = HexColor('#FDFEFD');
-  static HexColor skyBlue = HexColor('#3EB7F4');
-  static HexColor cocoa = HexColor('#6D4A09');
-  static HexColor steelBlack = HexColor('#2A2A2A');
-  static HexColor metalBlue = HexColor('#075881');
-  static HexColor mandarinOrange = HexColor('#F7B364');
+  static Color duckyYellow = const Color(0xFFF7DE64);
+  static Color caramelCheese = const Color(0xFFFFF3B7);
+  static Color whippedCream = const Color(0xFFFFFCF0);
+  static Color frostWhite = const Color(0xFFFDFEFD);
+  static Color skyBlue = const Color(0xFF3EB7F4);
+  static Color cocoa = const Color(0xFF6D4A09);
+  static Color steelBlack = const Color(0xFF2A2A2A);
+  static Color metalBlue = const Color(0xFF075881);
+  static Color mandarinOrange = const Color(0xFFF7B364);
 }
 
 class DuckDuckStatus {
-  static HexColor success = HexColor('#83F53D');
-  static HexColor warning = HexColor('#F79964');
-  static HexColor error = HexColor('#F85454');
-  static HexColor disabled = HexColor('#E5E6E5');
-  static HexColor disabledForeground = HexColor('#CBCDCB');
+  static Color success = const Color(0xFF83F53D);
+  static Color warning = const Color(0xFFF79964);
+  static Color error = const Color(0xFFF85454);
+  static Color disabled = const Color(0xFFE5E6E5);
+  static Color disabledForeground = const Color(0xFFCBCDCB);
 }
