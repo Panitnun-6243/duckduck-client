68e9e8c04aea9562c366febc2dc81f619b8541b7
Panitnun-6243 <panitnun.6243@gmail.com>
2023-10-14 20:49:33 +0700

    feat: draft color temperature

--- lib/widgets/light_control/light_color_picker.dart
+++ lib/widgets/light_control/light_color_picker.dart
@@ -17,11 +17,22 @@ class _LightColorPickerState extends State<LightColorPicker> {
   // create some values
   Color pickerColor = const Color(0xfff2DB6F);
   Color currentColor = const Color(0xfff2DB6F);
+  Color tempColor = const Color(0xfff2DB6F);
 
   TextEditingController textColorController = TextEditingController();
 // ValueChanged<Color> callback
   void changeColor(Color color) {
-    setState(() => pickerColor = color);
+    setState(() {
+      changeTempColor(color);
+      pickerColor = color;
+    });
+  }
+
+  void changeTempColor(Color color) {
+    setState(() {
+      // changeColor(color);
+      tempColor = color;
+    });
   }
 
   void saveColor() {
@@ -41,76 +52,124 @@ class _LightColorPickerState extends State<LightColorPicker> {
     showDialog(
       context: context,
       builder: (BuildContext context) {
-        return AlertDialog(
+        return Dialog(
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(15),
           ),
-          title: Text(
-            'Pick a color!',
-            style: GoogleFonts.rubik(
-                fontSize: 20,
-                fontWeight: FontWeight.w500,
-                color: DuckDuckColors.mandarinOrange),
-          ),
-          content: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.only(
+              left: 22,
+              right: 22,
+              top: 22,
+              bottom: 10,
+            ),
             child: Column(
               children: [
-                ColorPicker(
-                  enableAlpha: true,
-                  displayThumbColor: true,
-                  pickerColor: pickerColor,
-                  onColorChanged: changeColor,
-                  pickerAreaHeightPercent: 0.8,
-                  paletteType: PaletteType.hsl,
-                  labelTypes: const [
-                    ColorLabelType.hex,
-                    ColorLabelType.rgb,
-                    ColorLabelType.hsl,
-                    ColorLabelType.hsv
-                  ],
-                  pickerAreaBorderRadius: const BorderRadius.all(
-                    Radius.circular(10),
-                  ),
-                  hexInputController: textColorController,
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
-                  child: CupertinoTextField(
-                    controller: textColorController,
-                    prefix: const Padding(
-                        padding: EdgeInsets.only(left: 8),
-                        child: Icon(Icons.tag)),
-                    suffix: IconButton(
-                      icon: const Icon(Icons.content_paste_rounded),
-                      onPressed: () =>
-                          copyToClipboard(textColorController.text),
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            'Pick a color',
+                            style: GoogleFonts.rubik(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w500,
+                                color: DuckDuckColors.mandarinOrange),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 18,
+                        ),
+                        ColorPicker(
+                          enableAlpha: true,
+                          displayThumbColor: true,
+                          pickerColor: pickerColor,
+                          onColorChanged: changeColor,
+                          pickerAreaHeightPercent: 0.8,
+                          paletteType: PaletteType.hsl,
+                          labelTypes: const [
+                            ColorLabelType.hex,
+                            ColorLabelType.rgb,
+                            ColorLabelType.hsl,
+                            ColorLabelType.hsv
+                          ],
+                          pickerAreaBorderRadius: const BorderRadius.all(
+                            Radius.circular(10),
+                          ),
+                          hexInputController: textColorController,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
+                          child: CupertinoTextField(
+                            controller: textColorController,
+                            prefix: const Padding(
+                                padding: EdgeInsets.only(left: 8),
+                                child: Icon(Icons.tag)),
+                            suffix: IconButton(
+                              icon: const Icon(Icons.content_paste_rounded),
+                              onPressed: () =>
+                                  copyToClipboard(textColorController.text),
+                            ),
+                            autofocus: true,
+                            maxLength: 9,
+                            inputFormatters: [
+                              UpperCaseTextFormatter(),
+                              FilteringTextInputFormatter.allow(
+                                  RegExp(kValidHexPattern)),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            'Temperature',
+                            style: GoogleFonts.rubik(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w500,
+                                color: DuckDuckColors.mandarinOrange),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 18,
+                        ),
+                        SlidePicker(
+                          pickerColor: tempColor,
+                          enableAlpha: false,
+                          onColorChanged: changeColor,
+                          displayThumbColor: true,
+                          showParams: true,
+                          showIndicator: true,
+                          indicatorBorderRadius: const BorderRadius.vertical(
+                              top: Radius.circular(20)),
+                        )
+                      ],
                     ),
-                    autofocus: true,
-                    maxLength: 9,
-                    inputFormatters: [
-                      UpperCaseTextFormatter(),
-                      FilteringTextInputFormatter.allow(
-                          RegExp(kValidHexPattern)),
-                    ],
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                    child: Text(
+                      'Save',
+                      style: GoogleFonts.rubik(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          color: DuckDuckColors.mandarinOrange),
+                    ),
+                    onPressed: () {
+                      saveColor();
+                    },
                   ),
                 ),
               ],
             ),
           ),
-          actions: <Widget>[
-            TextButton(
-              child: Text(
-                'Save',
-                style: GoogleFonts.rubik(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w500,
-                    color: DuckDuckColors.mandarinOrange),
-              ),
-              onPressed: () {
-                saveColor();
-              },
-            ),
-          ],
         );
       },
     );
