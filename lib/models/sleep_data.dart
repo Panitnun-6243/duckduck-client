17a025e2e0446de590faa4473401c31ac9744bbd
Panitnun-6243 <panitnun.6243@gmail.com>
2023-11-26 18:49:57 +0700

    feat: add sleep statistics chart

---
+++ lib/models/sleep_data.dart
@@ -0,0 +1,6 @@
+class SleepData {
+  final String date;
+  final double sleepDurationHours;
+
+  SleepData({required this.date, required this.sleepDurationHours});
+}
--- lib/widgets/sleep/analysis_graph.dart
+++ lib/widgets/sleep/analysis_graph.dart
@@ -2,25 +2,91 @@ import 'package:duckduck/utils/colors.dart';
 
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+
+import '../../models/sleep_data.dart';
 
 class AnalysisGraph extends StatefulWidget {
-  AnalysisGraph({super.key});
-
-  final Color barBackgroundColor = DuckDuckStatus.disabled;
-  final Color barColor = DuckDuckColors.skyBlue;
-  final Color touchedBarColor = DuckDuckColors.duckyYellow;
+  const AnalysisGraph({super.key});
 
   @override
   State<AnalysisGraph> createState() => _AnalysisGraphState();
 }
 
 class _AnalysisGraphState extends State<AnalysisGraph> {
-  final Duration animDuration = const Duration(milliseconds: 250);
-  int touchedIndex = -1;
+  late TooltipBehavior _tooltipBehavior;
+  late List<ChartData> chartData;
+  List<SleepData> rawData = [
+    SleepData(date: "2023-11-21", sleepDurationHours: 7),
+    SleepData(date: "2023-11-22", sleepDurationHours: 5),
+    SleepData(date: "2023-11-23", sleepDurationHours: 3.5),
+    SleepData(date: "2023-11-24", sleepDurationHours: 7),
+    SleepData(date: "2023-11-25", sleepDurationHours: 5),
+    SleepData(date: "2023-11-26", sleepDurationHours: 6.5),
+  ];
 
-  bool isPlaying = false;
+  List<ChartData> processSleepData(List<SleepData> rawData) {
+    rawData.sort(
+      (a, b) => DateTime.parse(b.date).compareTo(
+        DateTime.parse(a.date),
+      ),
+    );
+    List<SleepData> latestData = rawData.take(7).toList();
+
+    // Map to ChartData objects
+    return latestData
+        .map((sleepData) {
+          DateTime date = DateTime.parse(sleepData.date);
+          String dayOfWeek = _getDayOfWeek(date);
+          return ChartData(dayOfWeek, sleepData.sleepDurationHours);
+        })
+        .toList()
+        .reversed
+        .toList(); // Reverse to make the chart start from the oldest of the 7 days
+  }
+
+  String _getDayOfWeek(DateTime date) {
+    List<String> days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+    return days[date.weekday - 1];
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _tooltipBehavior = TooltipBehavior(enable: true);
+    chartData = processSleepData(rawData);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(aspectRatio: 2, child: Container());
+    return SfCartesianChart(
+      primaryXAxis: CategoryAxis(),
+      tooltipBehavior: _tooltipBehavior,
+      title: ChartTitle(
+        text: 'Statistics (hours)',
+        textStyle: GoogleFonts.rubik(
+            fontSize: 10,
+            fontWeight: FontWeight.w400,
+            color: DuckDuckColors.steelBlack),
+        alignment: ChartAlignment.near,
+      ),
+      series: <ChartSeries<ChartData, String>>[
+        ColumnSeries<ChartData, String>(
+          color: DuckDuckColors.skyBlue,
+          enableTooltip: true,
+          dataSource: chartData,
+          xValueMapper: (ChartData data, _) => data.x,
+          yValueMapper: (ChartData data, _) => data.y,
+          spacing: 0.1,
+          borderRadius: const BorderRadius.all(Radius.circular(15)),
+        )
+      ],
+    );
   }
+}
+
+class ChartData {
+  ChartData(this.x, this.y);
+  final String x;
+  final double y;
 }
